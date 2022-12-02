8acc1087b2b2ef8ede3f7cbbd3fe3a969c21108b
Ogiji Max <107295436+MaxiTeddy@users.noreply.github.com>
2022-12-02 11:47:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("nav-bar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  var 
